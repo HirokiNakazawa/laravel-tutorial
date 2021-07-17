6f724bc58a017a485438a6c26564026b17da9cc6
HirokiNakazawa <sp.hn_hc4@i.softbank.jp>
2021-07-17 22:44:43 +0900

    5-8

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', [HelloController::class, 'index']);
-    
+Route::get('hello', [HelloController::class, 'index']);    
 Route::post('hello', [HelloController::class, 'post']);
+
+Route::get('hello/add', [HelloController::class, 'add']);
+Route::post('hello/add', [HelloController::class, 'create']);
